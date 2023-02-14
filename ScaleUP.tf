4b8d251631ab2d96518d15b5d589c7d74a2f58e4
h3itham <eng.haithamelabd@gmail.com>
2023-02-15 01:22:50 +0200

    Adding Scalling policies

---
+++ ScaleUP.tf
@@ -0,0 +1,23 @@
+
+resource "aws_autoscaling_policy" "web_policy_up" {
+  name = "web_policy_up"
+  scaling_adjustment = 1
+  adjustment_type = "ChangeInCapacity"
+  cooldown = 300
+  autoscaling_group_name = aws_autoscaling_group.web.name
+}
+resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_up" {
+  alarm_name = "web_cpu_alarm_up"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods = "2"
+  metric_name = "CPUUtilization"
+  namespace = "AWS/EC2"
+  period = "120"
+  statistic = "Average"
+  threshold = "60"
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.web.name
+  }
+  alarm_description = "This metric monitor EC2 instance CPU utilization"
+  alarm_actions = [ aws_autoscaling_policy.web_policy_up.arn ]
+}
