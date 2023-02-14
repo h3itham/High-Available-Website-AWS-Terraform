ba1c11588443089a3bee47650b474efaff08922e
h3itham <eng.haithamelabd@gmail.com>
2023-02-15 01:22:06 +0200

    Creating http security group

---
+++ securityGroup.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound connections"
+  vpc_id = aws_vpc.my_vpc.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22 
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Allow HTTP Security Group"
+  }
+
+}
